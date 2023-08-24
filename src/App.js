b64d2ae20faf887a03398421afab57d55f1d6f37
ASHISHTOMER0817 <ashish0817tomer@gmail.com>
2023-08-25 00:07:12 +0530

    successfully started tailwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Body from './components/Body'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Body from "./components/Body";
 function App() {
   return (
     <div className="App">
-<Header/>
-<Body/>
-<Footer/>
+      <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.js"], // Use the ** wildcard to include all files in subdirectories
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
